c4e9790ac185a8636175d10fbe7a9d1f4fd14fc2
Davydenkov Mihail <davydenkov_m@mail.ru>
2015-10-16 19:53:28 +0300

    add strage_fizzbuzz script

---
+++ anonymous_functions/anonymous_fizzbuzz.exs
@@ -0,0 +1,12 @@
+strange_fizzbuzz = fn
+  0, 0, _ -> "FizzBuzz"
+  0, _, _ -> "Fizz"
+  _, 0, _ -> "FizzBuzz"
+  _, _, third_arg -> "#{third_arg}"
+end
+
+fizzbuzz = fn n ->
+  strange_fizzbuzz.(rem(n,3), rem(n,5), n)
+end
+
+IO.puts Enum.map(1..10, &fizzbuzz.(&1))
