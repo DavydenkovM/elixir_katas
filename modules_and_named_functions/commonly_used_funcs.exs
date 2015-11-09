43e10a8c56ad67a34624af40e51b4565da95deca
Davydenkov Mihail <davydenkov_m@mail.ru>
2015-11-09 14:05:41 +0300

    modules_and_functioins: add some commonly user_funcs.exs

---
+++ modules_and_named_functions/commonly_used_funcs.exs
@@ -0,0 +1,32 @@
+defmodule CommonlyUsedFuncs do
+  def float_to_string(number) do
+    IO.puts Float.to_string(number, [decimals: 2, compact: true])
+  end
+
+  def get_env do
+    System.put_env("ENV1", "sample_env_value")
+    IO.puts System.get_env("ENV1")
+  end
+
+  def extname(path) do
+    IO.puts Path.extname(path)
+  end
+
+  def pwd do
+    IO.puts elem(File.cwd, 1)
+  end
+end
+
+# 1
+CommonlyUsedFuncs.float_to_string(3.14)
+CommonlyUsedFuncs.float_to_string(3.145)
+CommonlyUsedFuncs.float_to_string(3.105)
+
+# 2
+CommonlyUsedFuncs.get_env
+
+# 3
+CommonlyUsedFuncs.extname("~/dotfiles/.tmux.conf")
+
+# 4
+CommonlyUsedFuncs.pwd
